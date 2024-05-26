aecf20b1ff0d736ced79abcc8426291b9d1e4414
rubiyet <rubiyetfardous@gmail.com>
2024-05-26 23:16:14 +0600

    navbar add

--- src/components/common/layout/navbar/index.js
+++ src/components/common/layout/navbar/index.js
@@ -1,8 +1,13 @@
+import Image from 'next/image';
+import Logo from '@/assets/images/logo.png';
+
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="bg-white">
+    <div className="w-9/12 m-auto h-[68px] flex items-center">
+      <Image src={Logo} alt="logo" width={144} height={40} />
     </div>
+  </div>
   );
 }
 
